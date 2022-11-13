19f3db34aaa1aad9a3cf58a7623dabd5c45bb93f
Basia <b4605634@gmail.com>
2022-11-13 13:58:38 +0100

    Set up of driver, add lockback and hooks

---
+++ src/test/java/Pages/HomePage.java
@@ -0,0 +1,8 @@
+package Pages;
+
+public class HomePage {
+    ///webElement
+    //PageFactory
+    //operacje na webElementach
+
+}
---
+++ src/test/java/SiiShop/SiiShopLoginTest.java
@@ -0,0 +1,15 @@
+package SiiShop;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+public class SiiShopLoginTest {
+    private WebDriver driver;
+
+    @Test
+    void loginToApplication(){
+        //0. Konfiguracja WebDrivera
+        String browser = "chrome";
+    }
+
+}
